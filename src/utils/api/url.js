842374636566e2d1eeee3f91d31261c2380d542c
muhammaddiyor <muhammaddiyor20032808@gmail.com>
2024-07-29 10:56:32 +0500

    base url

--- src/utils/api/url.js
+++ src/utils/api/url.js
@@ -1,6 +1,6 @@
-// const BASE_URL = "https://express-backend-9nif.onrender.com/";
-const BASE_URL = "http://localhost:5003/";
+const SERVER_URL = "https://express-backend-9nif.onrender.com/";
+const LOCAL_URL = "http://localhost:5003/";
 
-// export const url = process.env.NODE_ENV === "production" ? serverURL : localUrl;
+const url = process.env.NODE_ENV === "production" ? SERVER_URL : LOCAL_URL;
 
-export default BASE_URL;
+export default url;
